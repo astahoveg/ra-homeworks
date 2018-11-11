441784241a60ea7f232c1be4d2523393cfe9f447
Astahov Evgeniy <astahov.evgeny@gmail.com>
2018-11-11 23:30:53 +0300

    Добавляет React-компонент Menu, который реализовывает интерфейс выпадающего меню в раскрытом виде, или в закрытом

---
+++ jsx/menu/js/Menu.js
@@ -0,0 +1,18 @@
+const Menu = function (props) {
+    const { items, opened } = props;
+    const item_list = items.map((item) => <li><a href={item.href}>{item.title}</a></li>);
+    const navigation = (
+        <nav>
+            <ul>{item_list}</ul>
+        </nav>
+    );
+
+    return (
+        <div className={(opened) ? "menu menu-open" : "menu"}>
+            <div className="menu-toggle">
+                <span />
+            </div>
+            {(opened) ? navigation : null}
+        </div>
+    );
+}
