bc0ff6c86f8555a84f51fe31a687d25baaa10db2
Astahov Evgeniy <astahov.evgeny@gmail.com>
2018-11-13 22:52:30 +0300

    Добавляет реализацию компонента отображающего рейтинг фильма в виде звезд

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,19 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+function Stars(props) {
+  const count = props.count;
+  let arrayStars = [];
+  
+  if ((typeof count != "number")||(count < 1)||(count > 5)) {
+    return null;
+  }
+  
+  for (let i=0; i<count; i++) {
+    arrayStars.push(i+1);
+  }
+
+  const starList = arrayStars.map((item) => <li key={item}><Star /></li>);
+  return (
+    <ul className="card-body-stars u-clearfix">{starList}</ul>
+  );
 }
