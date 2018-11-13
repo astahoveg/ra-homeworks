31f4fa3a6cdd4d2580176c140c2ad0fcdc92d6f4
Astahov Evgeniy <astahov.evgeny@gmail.com>
2018-11-13 23:43:05 +0300

    Реализация компонета история сообщений для чата

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function MessageHistory(props) {
+    if (messageList.length == 0) {
+        return null;
+    }
+
+    const MessagesChat = props.list.map((item) => {
+        let MessageChat;
+
+        if (item.type == 'response') {
+            MessageChat = Response;
+        } else if (item.type == 'message') {
+            MessageChat = Message;
+        } else {
+            MessageChat = Typing;
+        }
+
+        return <MessageChat key={item.id} from={item.from} message={item} />;
+    });
+
+    return (
+        <ul>
+            {MessagesChat}
+        </ul>
+    );
+}
--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -3,17 +3,17 @@
 function Stars(props) {
   const count = props.count;
   let arrayStars = [];
-  
-  if ((typeof count != "number")||(count < 1)||(count > 5)) {
+
+  if ((typeof count != "number") || (count < 1) || (count > 5)) {
     return null;
   }
-  
-  for (let i=0; i<count; i++) {
-    arrayStars.push(i+1);
+
+  for (let i = 0; i < count; i++) {
+    arrayStars.push(i + 1);
   }
 
-  const starList = arrayStars.map((item) => <li key={item}><Star /></li>);
+  const StarList = arrayStars.map((item) => <li key={item}><Star /></li>);
   return (
-    <ul className="card-body-stars u-clearfix">{starList}</ul>
+    <ul className="card-body-stars u-clearfix">{StarList}</ul>
   );
 }
