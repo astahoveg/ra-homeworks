b059216f6a18cebc739252fa59c1a081d4afd0c5
Astahov Evgeniy <astahov.evgeny@gmail.com>
2018-11-27 21:00:25 +0300

    Реализация фильтра в библиотеке

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,15 @@
-const SearchBox = () => {
-    return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
-    );
+class SearchBox extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    onChange = (event) => {
+        this.props.filterBooks(event.target.value);
+    };
+
+    render() {
+        return (
+            <input type="text" placeholder="Поиск по названию или автору" defaultValue={this.props.value} onChange={this.onChange} />
+        );
+    };
 };
--- ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -1,30 +1,30 @@
 class SubscribeForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        validEmail : true
-      };
-    }
-    
-    onChange = (event) => {
-      this.setState({validEmail : event.currentTarget.validity.valid});
+  constructor(props) {
+    super(props);
+    this.state = {
+      validEmail: true
     };
-    
-    render() {
-      return (
-        <div className="subscribe__form">
-          <form className={`form form--subscribe ${(this.state.validEmail)? 'is-valid' : 'is-error'}`}>
-            <h4 className="form-title">Подписаться:</h4>
-            <div className="form-group">
-              <label for="input-email" className="sr-only">Email</label>
-              <input type="email" id="input-email" placeholder="Email" className="form-control" onChange={this.onChange}/>
-              <div className="form-error">Пожалуйста, проверьте корректность адреса электронной почты</div>
-              <button type="submit" className="form-next">
-                <i className="material-icons">keyboard_arrow_right</i>
-              </button>
-            </div>
-          </form>
-        </div>
-      );
-    }
   }
+
+  onChange = (event) => {
+    this.setState({ validEmail: event.currentTarget.validity.valid });
+  };
+
+  render() {
+    return (
+      <div className="subscribe__form">
+        <form className={`form form--subscribe ${(this.state.validEmail) ? 'is-valid' : 'is-error'}`}>
+          <h4 className="form-title">Подписаться:</h4>
+          <div className="form-group">
+            <label for="input-email" className="sr-only">Email</label>
+            <input type="email" id="input-email" placeholder="Email" className="form-control" onChange={this.onChange} />
+            <div className="form-error">Пожалуйста, проверьте корректность адреса электронной почты</div>
+            <button type="submit" className="form-next">
+              <i className="material-icons">keyboard_arrow_right</i>
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
