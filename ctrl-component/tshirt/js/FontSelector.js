7e5692a56c3dcf8ebd3ef4e4786fd8d31d900014
Astahov Evgeniy <astahov.evgeny@gmail.com>
2018-11-27 22:10:18 +0300

    Реализация конструктора футболки

--- ctrl-component/tshirt/js/FontSelector.js
+++ ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,22 @@
-const FontSelector = ({fonts, selectedFont, onSelect}) => {
+const FontSelector = ({ fonts, selectedFont, onSelect }) => {
+    const defStr = "abc";
     return (
-        <div className="font-picker">
-            Выберите шрифт
+        <div className="grid center font-item">
+            {fonts.map(font => {
+                return (
+                    <React.Fragment>
+                        <input type="radio" 
+                               name="font" 
+                               value={font.name} 
+                               id={font.name} 
+                               selected={((selectedFont)&&(font.path == selectedFont.name))} 
+                               onChange={() => onSelect(font)} />
+                        <label for={font.name} className="grid-1">
+                            <PictureFont text={defStr} path={font.path} />
+                        </label>
+                    </React.Fragment>
+                );
+            })}
         </div>
-    )
+    );
 };
