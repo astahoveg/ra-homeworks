fad22d939e8a790236dd3842f1dfc06638c7e3c0
Astahov Evgeniy <astahov.evgeny@gmail.com>
2018-12-02 19:26:56 +0300

    Реализация фиксации строки поиска

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -2,18 +2,39 @@ class SearchBox extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { fixed: false };
+    this.state = { fixed: false, defaultTop: 0 };
   }
 
   render() {
     return <SearchBoxView fixed={this.state.fixed} />
   }
 
-  isFixed() {
-    return undefined;
+  componentDidMount() {
+    this.setState({ defaultTop: this.getTopElement() });
+    window.addEventListener("scroll", this.setPosition);
   }
 
-  setPosition() {
-    return undefined;
+  shouldComponentUpdate(nextProps, nextState) {
+    return (nextState.fixed != this.state.fixed);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.setPosition);
+  }
+
+  getTopElement() {
+    const elem = document.querySelector(".search-box");
+    return elem.getBoundingClientRect().top;
+  }
+
+  isFixed() {
+    const scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    return ((this.state.defaultTop - scrolled) <= 0);
+  }
+
+  setPosition = () => {
+    this.setState({
+      fixed: this.isFixed()
+    });
   }
 }
