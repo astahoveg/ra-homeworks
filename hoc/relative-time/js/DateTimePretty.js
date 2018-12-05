94d885085f58f693f4865d3e14470e88eb8f07ba
Evgeny <astahov_eg@uDesktop-16.04.5>
2018-12-05 22:04:24 +0300

    Изменение даты публикации

---
+++ hoc/relative-time/js/DateTimePretty.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//12 минут назад если прошло меньше часа, 5 часов назад если прошло больше часа, X дней назад если больше суток.
+function strToDate(str) {
+  return new Date(str.substr(0,4),+str.substr(5,2)-1,str.substr(8,2),str.substr(11,2),str.substr(14,2),str.substr(17,2));
+}
+
+function replaceDate(date) {
+    const nowDate = new Date();
+    const difference = nowDate - date;
+    const min = 60000;
+    const hour = 3600000;
+    const day = 86400000;
+    let str = "";
+    
+    if (difference < hour) { //Прошло меньше часа
+        str = `${Math.round(difference/min)} минут назад`;
+    } else if (difference < day) { // Прошло меньше суток
+        str = `${Math.round(difference/hour)} часов назад`;
+    } else { // Прошло больше суток
+        str = `${Math.round(difference/day)} дней назад`;
+    }
+
+    return str;
+}
+
+function strDateTime(Component) {
+    return function (props, ...args) {
+        console.log("strDateTime", props);
+        props.date = replaceDate(strToDate(props.date));
+        console.log("strDateTime new", props);
+        return Component.apply(this, [props, ...args]);
+    }
+}
+
+const DateTimePretty = strDateTime(DateTime);
