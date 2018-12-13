7af1123491aa2e547c01b73c74e7c8667adbfdaf
Evgeny <astahov_eg@uDesktop-16.04.5>
2018-12-14 02:04:58 +0300

    up

--- other-libs/gmaps/js/Map.js
+++ other-libs/gmaps/js/Map.js
@@ -1,10 +1,25 @@
 class Map extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      points: [],
+    };
+  }
+
+  componentWillMount() {
+    this.setState({ points: this.props.points });
+  };
+  
   render() {
+    console.log("render", this.props, this.props.points, this.state);
     return (
-      <div>
-        Карта
-        {this.props.points.map(office => <p>{JSON.stringify(office)}</p>)}
-      </div>
+      <div id="map" />
     );
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount", this.props, this.props.points, this.state);
+	  const map = new google.maps.Map(document.getElementById("map"));
   }
 }
